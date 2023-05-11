7e485c371dd48d4262ed30ede0f1973de49a1adc
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-05-11 12:07:00 +0300

    ""

--- App.py
+++ App.py
@@ -1,3 +1,28 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+
+class CalculatorLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 4
+
+        self.add_widget(Button(text='7'))
+        self.add_widget(Button(text='8'))
+        self.add_widget(Button(text='9'))
+        self.add_widget(Button(text='+''))
+
+        self.add_widget(Button(text='4'))
+        self.add_widget(Button(text='5'))
+        self.add_widget(Button(text='6'))
+        self.add_widget(Button(text='-'))
+
+        self.add_widget(Button(text='1'))
+        self.add_widget(Button(text='2'))
+        self.add_widget(Button(text='3'))
+        self.add_widget(Button(text='*'))
+
+        self.add_widget(Button(text='.'))
+        self.add_widget(Button(text='0'))
+        self.add_widget(Button(text='C'))
+        self.add_widget(Button(text='/'))
